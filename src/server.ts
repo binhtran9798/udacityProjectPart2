cc2f11c7983cdf9e6bc43378266f67a88d6f001f
emaildehoc2019 <emaildehoc2019@gmail.com>
2023-02-26 11:31:10 +0700

    implement restful endpoint

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
@@ -28,6 +28,18 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
   /**************************************************************************** */
+
+  app.get( "/filteredimage", async ( req: Request, res: Response ) => {
+    const {image_url} = req.query;
+    if(!image_url) {
+      return res.status(400)
+                  .send(`image url is required`);
+    }
+    const local_img = await filterImageFromURL(image_url);
+    return res.status(200).sendFile(local_img, () => {
+      deleteLocalFiles([local_img]);
+    })
+  } );
 
   //! END @TODO1
   
